ed40ad00587986aaea7b515adae6e887bb7f8e5a
chriswebdv <chriswebdv@gmail.com>
2024-06-13 06:37:18 -0600

    first commit

---
+++ ch3-17.py
@@ -0,0 +1,19 @@
+def become_warrior(first_name, last_name, power):
+    # ?
+
+
+# Don't edit below this line
+
+
+def main():
+    test("Frodo", "Baggins", 5)
+    test("Bilbo", "Baggins", 10)
+    test("Gandalf", "The Grey", 9000)
+
+
+def test(first_name, last_name, power):
+    title, new_power = become_warrior(first_name, last_name, power)
+    print(title, "has a power level of:", new_power)
+
+
+main()
