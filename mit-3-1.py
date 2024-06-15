bceddc874e814b3cb3b005079c050894ebe855cf
chriswebdv <chriswebdv@gmail.com>
2024-06-15 11:17:02 -0600

    Add counter variable to the while loop to keep track of the times the user inputs right

--- mit-3-1.py
+++ mit-3-1.py
@@ -2,4 +2,6 @@ where = input("Go left or right: ")
 counter = 0
 while where == "right":
     where = input("Go left or right: ")
+    counter = counter + 1
+    print(counter)
 print("you are out!")
