6eca95c64ea78611176bd41ba378319e43f28567
chriswebdv <chriswebdv@gmail.com>
2024-06-15 11:15:14 -0600

    fix the print statement by adding a space at the end of the line

--- mit-3-1.py
+++ mit-3-1.py
@@ -1,5 +1,5 @@
-where = input("Go left or right")
+where = input("Go left or right: ")
 counter = 0
 while where == "right":
-    where = input("Go left or right")
+    where = input("Go left or right: ")
 print("you are out!")
