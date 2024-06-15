8e0233ec8b851251c5bcc7d437350bb5492aaaa9
chriswebdv <chriswebdv@gmail.com>
2024-06-15 11:19:31 -0600

    Add if statement to keep track when the loop goes over by 2 times

--- mit-3-1.py
+++ mit-3-1.py
@@ -4,4 +4,6 @@ while where == "right":
     where = input("Go left or right: ")
     counter = counter + 1
     print(counter)
+    if counter == 2:
+        print("Sad face :()")
 print("you are out!")
