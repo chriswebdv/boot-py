adebed5e3312e9bdf05c9b0fdb50404435727943
chriswebdv <chriswebdv@gmail.com>
2024-06-17 08:13:55 -0600

    Create conditional to filter out the repeat letter from the output

--- mit-4-2.py
+++ mit-4-2.py
@@ -2,7 +2,7 @@ s = "abca"
 letter = ""
 count = 0
 for char in s:
-   letter += char
-   print(letter)
-   if letter != char:
-      print(letter)
+    letter += char
+    if letter != char:
+      count += 1
+print(count)
