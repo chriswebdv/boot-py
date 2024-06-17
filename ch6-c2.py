485de24b8e69e06b6cdc03ebf69aae83ecb73b9e
chriswebdv <chriswebdv@gmail.com>
2024-06-17 17:53:03 -0600

    convert the rest of the binary values and return them within the function

--- ch6-c2.py
+++ ch6-c2.py
@@ -1,7 +1,9 @@
 def body_parts(num_heads, num_arms, num_fingers):
     num_heads = int(num_heads,2)
     print(num_heads)
-    return num_heads
+    num_arms = int(num_arms, 2)
+    num_fingers = int(num_fingers, 2)
+    return num_heads, num_arms, num_fingers
 
 run_cases = [
     ("1", "10", "1010", (1, 2, 10)),
