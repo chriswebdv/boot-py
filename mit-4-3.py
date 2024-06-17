684f114c70682a995575f536db4c6e7b6095a37d
chriswebdv <chriswebdv@gmail.com>
2024-06-17 09:41:11 -0600

    Add else to account when the number is not found

--- mit-4-3.py
+++ mit-4-3.py
@@ -1,7 +1,9 @@
 # find the secret number in a range
 
-secret_num = 15
+secret_num = 7
 
 for num in range(1, 11):
     if num == secret_num:
         print("secret number found!")
+    else:
+        print("not found")
