e95581809db191699b416cb94d14cb07286db0d1
chriswebdv <chriswebdv@gmail.com>
2024-06-20 16:14:29 -0600

    Ask user for input to get the number

--- number_guess.py
+++ number_guess.py
@@ -6,5 +6,8 @@ def numbers_game():
     for i in range(100):
         x = random.randint(1, 100)
         return x
-    
+
+ask_user = int(input("Guess my secret number: "))
+print(type(ask_user))
+
 print(numbers_game())
