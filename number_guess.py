75eb5bdc2f3f1dc6a1692c911476ba01417b6844
chriswebdv <chriswebdv@gmail.com>
2024-06-20 16:31:44 -0600

    Move code aroung and add F strings to get the randoms input on the screen

--- number_guess.py
+++ number_guess.py
@@ -1,22 +1,21 @@
 import random
 
 #Write a function with not arguments to guess a number from 1 - 100
-ask_user = int(input("Guess my secret number: "))
 
 def numbers_game():
     for i in range(100):
         x = random.randint(1, 100)
         return x
 
-print(numbers_game())
 
+ask_user = int(input("Guess my secret number: "))
 
 while ask_user != numbers_game():
     if ask_user > numbers_game():
-        print("Too high!")
+        print(f"The correct number is {numbers_game()} Too low!")
         ask_user = int(input("Guess my secret number: "))
     elif ask_user < numbers_game():
-        print("Too low!")
+        print(f"The correct number is {numbers_game()} Too high!")
         ask_user = int(input("Guess my secret number: "))
 
-
+print("Correcto Mundo! You guessed right")
