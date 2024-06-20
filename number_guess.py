8f7d916c07ef1e69e57a6aa06549ae88d8ec3b60
chriswebdv <chriswebdv@gmail.com>
2024-06-20 16:20:15 -0600

    Create while loop to compare the users guesses to the random function

--- number_guess.py
+++ number_guess.py
@@ -1,13 +1,18 @@
 import random
 
 #Write a function with not arguments to guess a number from 1 - 100
+ask_user = int(input("Guess my secret number: "))
 
 def numbers_game():
     for i in range(100):
         x = random.randint(1, 100)
         return x
 
-ask_user = int(input("Guess my secret number: "))
-print(type(ask_user))
-
 print(numbers_game())
+
+
+while ask_user != numbers_game():
+    if ask_user > numbers_game():
+        print("Too high!")
+
+
