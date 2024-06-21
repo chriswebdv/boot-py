b8e26080999cf5608a81178565f364225c626503
chriswebdv <chriswebdv@gmail.com>
2024-06-20 18:01:35 -0600

    Change the miles per gallon formula

--- ch7-c3.py
+++ ch7-c3.py
@@ -1,6 +1,11 @@
 def has_enough_gas(gallons_in_car, miles_one_way, miles_per_gallon):
-    gallons_needed = 2(miles_one_way * miles_per_gallon)
-    return gallons_needed
+    gallons_needed = 2 * (miles_one_way / miles_per_gallon)
+    print(gallons_needed)
+    print(gallons_in_car)
+    if gallons_in_car >= gallons_needed:
+        return True
+    else:
+        return False
 
 run_cases = [
     (8, 50, 22, True),
