2ad5ebc8b22620ead8cda50216f95ac08d014456
chriswebdv <chriswebdv@gmail.com>
2024-06-22 09:17:05 -0600

    first

---
+++ ch8-c1.py
@@ -0,0 +1,18 @@
+def countdown_to_blastoff():
+    # ?
+
+
+# Don't edit below this line
+
+
+def test():
+    print("Counting down to blastoff:")
+    countdown_to_blastoff()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
