a18ebe57539a8d20fec78f469d645a73f5232f00
chriswebdv <chriswebdv@gmail.com>
2024-06-22 16:48:24 -0600

    refactores the loops body

--- ch8-c3.py
+++ ch8-c3.py
@@ -1,7 +1,6 @@
 def calculate_experience_points(level):
-    count = 0
-    for i in range(0, level):
-        count += 1
-        i *= count
-        return i
+    for i in range(level):
+        i *= 5
+    return i * 2
 
+calculate_experience_points(100)
