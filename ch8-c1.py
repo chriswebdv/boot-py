93396e99a9fbe3dcd94339a05d60db291f770502
chriswebdv <chriswebdv@gmail.com>
2024-06-22 09:33:27 -0600

    Add else statement to complete the loop

--- ch8-c1.py
+++ ch8-c1.py
@@ -2,8 +2,8 @@ def countdown_to_blastoff():
     for i in range(10, 0, -1):
         if i == 1:
             print(f"{i}...Blastoff!")
-        
-        print(f"{i}...")
+        else:
+            print(f"{i}...")
 
 
 # Don't edit below this line
