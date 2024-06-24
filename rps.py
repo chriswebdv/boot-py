00848662f76309d372a46147f3c24ae438f4aa24
chriswebdv <chriswebdv@gmail.com>
2024-06-24 07:21:43 -0600

    create variables to keep track of the scores and create variable to get users input

--- rps.py
+++ rps.py
@@ -9,13 +9,19 @@ def computer_answer(answer):
         choice = random.choice(rps)
     return choice
 
-print(computer_answer(rps))
-
-
-
-
 # Create while loop that limits the user to 5 games and counts each game to determine a general winner 
 # of the tournament
 # inside the while loop create a conditional statement to detemine who wins each match and count the winners 
 # per match
 
+computer_score = 0
+human_score = 0
+count = 0
+
+print("-------------------------------")
+print("Choose between paper, rock or scissors, write in lowercase: ")
+human_input = input("Answer: ")
+print(human_input)
+
+while count <= 5:
+    pass
