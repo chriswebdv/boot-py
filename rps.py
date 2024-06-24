3c2805d1eeb6f0400d6734ad60d88f356dd19843
chriswebdv <chriswebdv@gmail.com>
2024-06-24 06:58:40 -0600

    create a list that includes 3 items: rock, paper and scissors also print to screen to test if it is working

---
+++ rps.py
@@ -0,0 +1,13 @@
+# create random function that returns rock, paper or scissors as output
+import random
+
+rps = ["rock", "paper", "sciccors"]
+
+print(rps)
+
+
+# Create while loop that limits the user to 5 games and counts each game to determine a general winner 
+# of the tournament
+# inside the while loop create a conditional statement to detemine who wins each match and count the winners 
+# per match
+
