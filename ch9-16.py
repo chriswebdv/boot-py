41350ba20d901a819e0abda0d4b8581121e744dc
chriswebdv <chriswebdv@gmail.com>
2024-06-26 16:22:55 -0600

    add a coditional to determine if a number is odd, add it to a list

--- ch9-16.py
+++ ch9-16.py
@@ -3,7 +3,8 @@ def get_odd_numbers(num):
 
     for i in range(0, num):
         # don't touch above this line
-        pass
+        if i % 2 != 0:
+            odd_numbers.append(i)
 
     # don't touch below this line
 
