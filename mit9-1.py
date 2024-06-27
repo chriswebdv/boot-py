1940b2eeaa7a59f4b14c92816f5a6f2d1f06ddbc
chriswebdv <chriswebdv@gmail.com>
2024-06-27 06:03:09 -0600

    create and else statement to return the consonants and return both values as a tuple

--- mit9-1.py
+++ mit9-1.py
@@ -1,9 +1,13 @@
 vowels = ("a", "e", "i", "o", "u")
 def char_counts(s):
     total_vowels = 0
+    total_consonants = 0
     for i in range(0, len(s)):
         if s[i] in vowels:
             total_vowels += len(s[i])
-    print(total_vowels)
+        else:
+            total_consonants += len(s[i])
 
-char_counts("tanko")
+    return (total_vowels, total_consonants)
+
+print(char_counts("mamacitas"))
