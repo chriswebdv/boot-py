d338f04411a5e6c421b25723272475f08bc379e4
chriswebdv <chriswebdv@gmail.com>
2024-06-27 08:10:41 -0600

    comment out the print statements and run the code

--- mit10-2.py
+++ mit10-2.py
@@ -1,14 +1,14 @@
 def remove_elem(my_list, num):
     my_list2 = []
     for i in my_list:
-        print("first",my_list2)
+        #print("first",my_list2)
         if i != num:
-            print("second", my_list2)
+            #print("second", my_list2)
             my_list2.append(i)
-            print("third", my_list2)
+            #print("third", my_list2)
     return my_list2
 
 
 my_list = ["word", 2, 2, 3, 4]
-num = 2
-remove_elem(my_list, num)
+num = 4
+print(remove_elem(my_list, num))
