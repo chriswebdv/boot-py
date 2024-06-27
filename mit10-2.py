14dd6e68566d062bf8771087572be65d625a4a4f
chriswebdv <chriswebdv@gmail.com>
2024-06-27 08:01:12 -0600

    change return name

--- mit10-2.py
+++ mit10-2.py
@@ -1,10 +1,12 @@
 def remove_elem(my_list, num):
-    for i in my_list:
-        if num in my_list:
-            return num
+    my_list2 = []
+    for i in range(len(my_list)):
+        if num != my_list[i]:
+            my_list2.append(num)
+
+    return my_list2
 
 
-
-
-
+my_list = ["word", 2, 2, 3, 4]
+num = 2
 print(remove_elem(my_list, num))
