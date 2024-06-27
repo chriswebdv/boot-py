bd3cb8f0d4f3a7047408083978b354bf11a100ad
chriswebdv <chriswebdv@gmail.com>
2024-06-27 08:07:28 -0600

    change the order of the variables

--- mit10-2.py
+++ mit10-2.py
@@ -1,12 +1,14 @@
 def remove_elem(my_list, num):
     my_list2 = []
-    for i in range(len(my_list)):
-        if num != my_list[i]:
-            my_list2.append(num)
-
+    for i in my_list:
+        print("first",my_list2)
+        if i != num:
+            print("second", my_list2)
+            my_list2.append(i)
+            print("third", my_list2)
     return my_list2
 
 
 my_list = ["word", 2, 2, 3, 4]
 num = 2
-print(remove_elem(my_list, num))
+remove_elem(my_list, num)
