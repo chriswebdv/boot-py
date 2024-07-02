ba3f5c7b68288da681f310632767d6c739fa90bb
chriswebdv <chriswebdv@gmail.com>
2024-07-02 07:43:20 -0600

    create a for loop and inside write a conditional to determine if a number is even or odd and acount both

--- ch9-C1.py
+++ ch9-C1.py
@@ -3,8 +3,12 @@ def get_odds_and_evens(numbers):
     num_evens = 0
 
     # Don't touch above this line
-
-    pass
+    for i in numbers:
+        if i % 2 == 0:
+            num_evens +=1
+        else:
+            num_odds += 1
+            
 
 
 run_cases = [
