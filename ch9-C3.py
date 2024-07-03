67308a1b27e9d0d725ae0fa066e1a7cd7bf9e8b6
chriswebdv <chriswebdv@gmail.com>
2024-07-02 21:12:13 -0600

    move total variable and return outside the loop

--- ch9-C3.py
+++ ch9-C3.py
@@ -3,10 +3,10 @@ def get_test_score(answer_sheet, student_answers):
     for i,j in zip(answer_sheet, student_answers):
         if i != j:
             count += 1
-            print("count", count)
-            total = (1 - (count / len(student_answers))) * 100
-            return total
-    
+
+    total = 100 - ((count / len(student_answers)) * 100)
+    return total
+                
 
 run_cases = [
     (
