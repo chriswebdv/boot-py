898fbbb33afe72fa9dc96558e58782dea9fe3011
chriswebdv <chriswebdv@gmail.com>
2024-07-05 17:06:21 -0600

    create loop to enter the right values inside my list

--- mit11-1.py
+++ mit11-1.py
@@ -1,7 +1,12 @@
 def remove_all(L, e):
-    pass
+    Lcopy = L[:]
+    L.clear()
+
+    for i in Lcopy:
+        if i != e:
+            L.append(i)
+    print(L)
 
 
 L = [1,2,2,2]
-remove_all(L, 2)
-print(L)
+remove_all(L, 1)
