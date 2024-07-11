b8579e985b84276456f9d9050178a07e2f02a137
chriswebdv <chriswebdv@gmail.com>
2024-07-11 08:02:03 -0600

    add if statement

--- ch10-7.py
+++ ch10-7.py
@@ -1,7 +1,8 @@
 def count_enemies(enemy_names):
     enemies_dict = {}
     for enemy_name in enemy_names:
-        enemies_dict[enemy_name] += 1
+        if enemy_name in enemy_names:
+            enemies_dict[enemy_name] += 1
     return enemies_dict
 
 
