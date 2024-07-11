41716e6bfa0fd1271b1995aa39982fa2738b90ba
chriswebdv <chriswebdv@gmail.com>
2024-07-10 21:00:24 -0600

    add kyes methods to my statement

--- mit14-2.py
+++ mit14-2.py
@@ -1,5 +1,11 @@
 def find_in_list(ld, k):
-    pass
+    if k in ld.keys():
+        return True
+    else:
+        return False
 
-
-print(find_in_list())
+d1 = {1:2, 3:4, 5:6}
+d2 = {2:4, 4:6}
+d3 = {1:1, 3:9, 4:16, 5:25}
+print(find_in_list([d1,d2,d3], 2))
+print(find_in_list([d1,d2,d3], 25))
