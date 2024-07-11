4b269eca7d57652ba376b17ccce24021c4ea706c
chriswebdv <chriswebdv@gmail.com>
2024-07-10 21:31:33 -0600

    add keys methos

--- mit14-2.py
+++ mit14-2.py
@@ -1,11 +1,14 @@
 def find_in_list(ld, k):
-    if k in ld.keys():
-        return True
-    else:
-        return False
+    for dict in ld:
+        for key in dict:
+            if k in dict.keys():
+                return True
+            else:
+                return False
+    
 
 d1 = {1:2, 3:4, 5:6}
 d2 = {2:4, 4:6}
 d3 = {1:1, 3:9, 4:16, 5:25}
 print(find_in_list([d1,d2,d3], 2))
-print(find_in_list([d1,d2,d3], 25))
+print(find_in_list([d1,d2,d3], 1))
