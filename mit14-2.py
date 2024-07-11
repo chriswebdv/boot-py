92075fa88b63b7e8fe5442ee769a6b38fd2ddbee
chriswebdv <chriswebdv@gmail.com>
2024-07-10 21:35:44 -0600

    refactored code

--- mit14-2.py
+++ mit14-2.py
@@ -1,14 +1,13 @@
 def find_in_list(ld, k):
     for dict in ld:
-        for key in dict:
-            if k in dict.keys():
-                return True
-            else:
-                return False
+        if k in dict:
+            return True
+    return False
+            
     
 
 d1 = {1:2, 3:4, 5:6}
 d2 = {2:4, 4:6}
 d3 = {1:1, 3:9, 4:16, 5:25}
-print(find_in_list([d1,d2,d3], 2))
+print(find_in_list([d1,d2,d3], 7))
 print(find_in_list([d1,d2,d3], 1))
