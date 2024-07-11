418130e17893607163689dbce0f0e1ff0f8840dc
chriswebdv <chriswebdv@gmail.com>
2024-07-10 21:49:29 -0600

    loop over dict to get the values

--- mit14-3.py
+++ mit14-3.py
@@ -1,10 +1,15 @@
 def count_matches(d):
-    pass
+    count = 0
+    for k,v in d.items():
+        if k == v:
+            count += 1
+    return count
+
 
 
 #Example
-d = {1:2, 3:4, 5:6}
+d = {1:2, 3:4, 5:6, 7:7}
 print(count_matches(d))
 
-d = {1:2, "a":"a", 5:5}
+d = {1:2, "a":"a", 5:5, "m":"m"}
 print(count_matches(d))
